e503d2947a88d9a77ade8dcf48a749c2a29bd880
georgivelikov <gmvelikov@gmail.com>
2017-02-12 16:07:18 +0200

    fix

--- server/routers/auth-router.js
+++ server/routers/auth-router.js
@@ -6,5 +6,4 @@ module.exports = ({ app, controllers }) => {
     app.post("/auth/register", controllers.auth.registerUser);
     app.post("/auth/login", controllers.auth.loginUser);
     app.post("/auth/logout", controllers.auth.logoutUser);
-    app.post("/auth/verify", controllers.auth.verifyLogin);
 };
