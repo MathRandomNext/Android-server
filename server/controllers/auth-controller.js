102ae9485a5c6c225f3ed2909c1de0abe07f0459
georgivelikov <gmvelikov@gmail.com>
2017-02-22 22:07:44 +0200

    Final fix

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -26,19 +26,19 @@ module.exports = () => {
 
             User.findOne({ username: body.username }, (err, user) => {
                 if (err) {
-                    res.json({errorMessage: "Unknown"});
-                    res.status(404);
+                    res.statusMessage = "Unknown";
+                    res.sendStatus(404);
                     return;
                 }
 
                 if (user) {
-                    res.json({ errorMessage: "Username already exists." });
-                    res.status(404);              
+                    res.statusMessage = "Username already exists.";
+                    res.sendStatus(404);              
                 } else {
                     User.create(body, (error, newUser) => {
                         if (error) {
-                            res.json({ errorMessage: "Enable to parse arguments." });
-                            res.status(404);
+                            res.statusMessage = "Enable to parse arguments.";
+                            res.sendStatus(404).end();
                         } else {
                             let result = {
                                 username: newUser.username,
@@ -46,7 +46,6 @@ module.exports = () => {
                             };
 
                             res.json({ result });
-                            res.status(200);
                         }                      
                     });
                 }
@@ -55,27 +54,27 @@ module.exports = () => {
         loginUser(req, res, next) {
             User.findOne({ username: req.body.username, passHash: req.body.passHash }, (err, user) => {
                 if (err) {
-                    throw err;
+                    res.statusMessage = "Unknown";
+                    res.sendStatus(400).end();
+                    return;
                 }
 
                 if (!user) {
-                    res.json({ errorMessage: "Invalid username or password." });
-                    res.status(404);
-                    
+                    res.statusMessage = "Invalid username or password.";
+                    res.sendStatus(404).end();                             
                 } else {
                     let result = {
                         username: user.username,
                         _id: user._id
                     };
-                    
+
                     res.json({ result });
-                    res.status(200);
                 }
             });
         },
         logoutUser(req, res) {
             req.logout();
-            res.status(200);
+            res.sendStatus(200);
         }
     };
 };
